211b7860cd1951ff3dc26e9879a2b5d480011935
Junya Qiao <junyaqiao@gmail.com>
2022-01-24 18:03:09 -0500

    create user and thought models

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+// username, email, thoughts, friends
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // use REGEX to validate email
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+})
+module.exports = User;
